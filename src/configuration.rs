c14379b27aeea5b228cdf13a964f2ba16b4de6ef
Diego Enriquez-Serrano <diego@diegoenriquezserrano.dev>
2025-12-17 12:03:23 -0600

    Adds configuration settings for setting custom session cookie name

--- src/configuration.rs
+++ src/configuration.rs
@@ -22,6 +22,7 @@ pub struct ApplicationSettings {
     pub host: String,
     #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
+    pub session_key: String,
 }
 
 #[derive(Deserialize, Clone)]
